eadbc91cf24c6eed4e5783225812b218f5e5f74b
Justin T <69059615+JustinSUF@users.noreply.github.com>
2023-10-30 13:57:49 -0400

    pause menu! broken! oh no!

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject pauseMenu;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,4 +17,22 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+    
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void ReturnMain()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneBuildIndex:0);
+    }
+
 }
